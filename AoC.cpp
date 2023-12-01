6670b16c151300a7270da57eaf107793aeeb2559
WhiteStorm <andrzejmmm1@gmail.com>
2023-12-01 03:13:40 +0100

    Empty Project
    Add Visual Studio ignore rules and create project files
    
    This commit adds a set of ignore rules for Visual Studio to the .gitignore file. It also creates new files for an empty C++ project, including AoC.cpp, AoC.sln, and AoC.vcxproj.

---
+++ AoC.cpp
@@ -0,0 +1,6 @@
+#include "Source/Main.h"
+
+int main()
+{
+	Main* obj = new Main();
+}
---
+++ Source/Main.cpp
@@ -0,0 +1,11 @@
+#include "Main.h"
+#include <iostream>
+
+Main::Main()
+{
+	std::cout << "Hello World!\n";
+}
+
+Main::~Main()
+{
+}
